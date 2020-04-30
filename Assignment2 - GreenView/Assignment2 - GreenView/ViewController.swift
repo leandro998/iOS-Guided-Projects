826e95eae8b383bf16e04b3b9ff02b0520db6619
Leandro Prado <57591950+leandro998@users.noreply.github.com>
2020-04-29 21:29:38 -0700

    Assignment 2 - autolayout drag & drop

---
+++ Assignment2 - GreenView/Assignment2 - GreenView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment2 - GreenView
+//
+//  Created by Leandro Prado on 2020-04-27.
+//  Copyright © 2020 Leandro Prado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
